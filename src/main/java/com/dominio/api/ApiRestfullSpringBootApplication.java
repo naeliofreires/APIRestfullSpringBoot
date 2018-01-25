06bdc4ee27abb3c50d9fa2df15927815f368f2eb
naeliofreires <naeliosistemas@gmail.com>
2018-01-25 14:40:00 -0300

    Arquivos Inicias do Projeto

---
+++ src/main/java/com/dominio/api/ApiRestfullSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.dominio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfullSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfullSpringBootApplication.class, args);
+	}
+}
